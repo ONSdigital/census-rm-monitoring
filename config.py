f60c4be42b3871b3becad30c1e10023e92eac8cf
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-07-30 14:20:15 +0100

    Cut out unused config

--- config.py
+++ config.py
@@ -13,36 +13,11 @@ class Config:
     DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_HOST_WRITE = os.getenv('DB_HOST_RW', 'localhost')
     DB_HOST_ACTION = os.getenv('DB_HOST_ACTION', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '6432')
-    DB_PORT_WRITE = os.getenv('DB_PORT_RW', '6432')
     DB_NAME = os.getenv('DB_NAME', 'postgres')
-    DB_NAME_WRITE = os.getenv('DB_NAME_RW', 'postgres')
     DB_USESSL = os.getenv('DB_USESSL', '')
     DB_ACTION_CERTIFICATES = os.getenv('DB_ACTION_CERTIFICATES',
                                        (" sslmode=verify-ca sslrootcert=/home/toolbox/.postgresql-action/root.crt "
                                         "sslcert=/home/toolbox/.postgresql-action/postgresql.crt "
                                         "sslkey=/home/toolbox/.postgresql-action/postgresql.key"))
-    CASEAPI_HOST = os.getenv('CASEAPI_HOST', 'localhost')
-    CASEAPI_PORT = os.getenv('CASEAPI_PORT', '8161')
-    EXCEPTIONMANAGER_HOST = os.getenv('EXCEPTIONMANAGER_HOST', 'localhost')
-    EXCEPTIONMANAGER_PORT = os.getenv('EXCEPTIONMANAGER_PORT', '8666')
-    EXCEPTIONMANAGER_URL = f'http://{EXCEPTIONMANAGER_HOST}:{EXCEPTIONMANAGER_PORT}'
-    QID_MODULUS = os.getenv('QID_MODULUS', '1')
-    QID_FACTOR = os.getenv('QID_FACTOR', '1')
-
-    BULK_WORKING_DIRECTORY = Path(os.getenv('BULK_WORKING_DIRECTORY', '/tmp'))
-
-    ENVIRONMENT = os.getenv('ENVIRONMENT')
-
-    EVENTS_EXCHANGE = os.getenv('EVENTS_EXCHANGE', 'events')
-
-
-class TestConfig(Config):
-    COLLECTION_EXERCISE_ID = os.getenv('COLLECTION_EXERCISE_ID', '88960972-38a5-438f-b543-cd9bb464f49a')
-    ACTION_PLAN_ID = os.getenv('ACTION_PLAN_ID', '88960972-38a5-438f-b543-cd9bb464f49a')
-
-
-if Config.ENVIRONMENT == 'TEST':
-    Config = TestConfig
