76a1603a5a291bf0ba7dd5cca34afc582c4f9867
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-01-28 08:16:09 +0000

    Add fulfilment to process count to action DB monitor

--- monitoring/action_worker_database_monitor.py
+++ monitoring/action_worker_database_monitor.py
@@ -4,8 +4,10 @@ from config import Config
 from utilities.db_helper import execute_sql_query
 
 if __name__ == "__main__":
-    sql_query = 'SELECT COUNT(*) FROM actionv2.case_to_process;'
-
-    db_result = execute_sql_query(sql_query, Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES)
-
-    print(json.dumps({'case_to_process_count': db_result[0][0]}))
+    case_to_process_count = 'SELECT COUNT(*) FROM actionv2.case_to_process;'
+    case_to_process_count_result = execute_sql_query(case_to_process_count, Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES)
+    print(json.dumps({'case_to_process_count': case_to_process_count_result[0][0]}))
+    
+    fulfilment_to_process_count = 'SELECT COUNT(*) FROM actionv2.fulfilment_to_process;'
+    fulfilment_to_process_count_result = execute_sql_query(fulfilment_to_process_count, Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES)
+    print(json.dumps({'fulfilment_to_process_count': fulfilment_to_process_count_result[0][0]}))
