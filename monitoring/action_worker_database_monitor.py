95ecb8047bac2e914a02d253d2637a0577991bba
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2021-03-05 09:43:28 +0000

    updated metric to print fulfilment type count (#9)
    * updated metric to print fulfilment type count
    
    * Removed unused line and fixed flake issue

--- monitoring/action_worker_database_monitor.py
+++ monitoring/action_worker_database_monitor.py
@@ -9,7 +9,12 @@ if __name__ == "__main__":
                                                      Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES)
     print(json.dumps({'case_to_process_count': case_to_process_count_result[0][0]}))
 
-    fulfilment_to_process_count = 'SELECT COUNT(*) FROM actionv2.fulfilment_to_process;'
+    fulfilment_to_process_count = 'SELECT fulfilment_code, COUNT(*) FROM actionv2.fulfilment_to_process ' \
+                                  'group by fulfilment_code;'
     fulfilment_to_process_count_result = execute_sql_query(fulfilment_to_process_count,
                                                            Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES)
-    print(json.dumps({'fulfilment_to_process_count': fulfilment_to_process_count_result[0][0]}))
+    fulfilment_dict = dict(fulfilment_to_process_count_result)
+    total_count = sum(fulfilment_dict.values())
+    print(json.dumps({'fulfilment_to_process_count': total_count}))
+    for fulfilment, count in fulfilment_dict.items():
+        print(json.dumps({'fulfilment': fulfilment, 'fulfilment_count': count}))
