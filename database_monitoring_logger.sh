3805561f2fe81a872ac3abd33fd6843650aeecc7
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2021-03-10 08:18:50 +0000

    2104 monitor for long running queries in dbs replicas (#10)
    * monitor long running DB queries
    
    * Updated query to not look for SET application_name

--- database_monitoring_logger.sh
+++ database_monitoring_logger.sh
@@ -2,5 +2,6 @@ while [ 1 ]
 do
     python -m monitoring.find_slow_event_processing
     python -m monitoring.action_worker_database_monitor
+    python -m monitoring.find_long_running_db_queries
     sleep 59s
 done
