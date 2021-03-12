61fab54d71257bd8214f9c0ac03069e385bcb56d
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-03-12 09:46:53 +0000

    Stop counting duplicate receipts (e.g. PQRS) (#11)

--- monitoring/regional_counts.py
+++ monitoring/regional_counts.py
@@ -34,11 +34,10 @@ def get_response_events():
                ELSE 'Unknown Region Responses' || counts.region END,
            counts.region_count
     FROM (SELECT LEFT(c.region,1) as region, COUNT(*) AS region_count
-          FROM casev2.event e, casev2.uac_qid_link q, casev2.cases c
-          WHERE e.event_type='RESPONSE_RECEIVED' AND e.uac_qid_link_id = q.id
-          AND q.active = 'f' AND q.caze_case_id = c.case_id
+          FROM casev2.uac_qid_link q, casev2.cases c
+          WHERE q.active = 'f' AND q.caze_case_id = c.case_id
           AND c.action_plan_id = 'c4415287-0e37-447b-9c3d-1a011c9fa3db' GROUP BY LEFT(c.region,1)) AS counts
-        """
+    """
 
     db_result = execute_sql_query(sql_query)
 
