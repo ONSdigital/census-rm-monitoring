c3afae8684850d34d054564fc0227436cc67c3eb
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-03-12 09:42:40 +0000

    Reduce cost of query by cutting some corners

--- monitoring/regional_counts.py
+++ monitoring/regional_counts.py
@@ -33,12 +33,11 @@ def get_response_events():
                WHEN 'N' THEN 'Northern Ireland Responses'
                ELSE 'Unknown Region Responses' || counts.region END,
            counts.region_count
-    FROM (SELECT LEFT(c.region,1) as region, COUNT(distinct(q.qid)) AS region_count
-          FROM casev2.event e, casev2.uac_qid_link q, casev2.cases c
-          WHERE e.event_type='RESPONSE_RECEIVED' AND e.uac_qid_link_id = q.id
-          AND q.active = 'f' AND q.caze_case_id = c.case_id
+    FROM (SELECT LEFT(c.region,1) as region, COUNT(*) AS region_count
+          FROM casev2.uac_qid_link q, casev2.cases c
+          WHERE q.active = 'f' AND q.caze_case_id = c.case_id
           AND c.action_plan_id = 'c4415287-0e37-447b-9c3d-1a011c9fa3db' GROUP BY LEFT(c.region,1)) AS counts
-        """
+    """
 
     db_result = execute_sql_query(sql_query)
 
