eef03d1ae8a5c5ac1d07d2400f3e2d1445639504
Ryan Grundy <ryan.grundy@ons.gov.uk>
2021-03-29 10:56:43 +0100

    Removed query from long running sql

--- monitoring/find_long_running_db_queries.py
+++ monitoring/find_long_running_db_queries.py
@@ -4,7 +4,7 @@ from config import Config
 from utilities.db_helper import execute_sql_query
 
 if __name__ == "__main__":
-    long_running_query_check = """SELECT pid, age(clock_timestamp(), query_start), usename, query, state
+    long_running_query_check = """SELECT pid, age(clock_timestamp(), query_start), usename, state
                                FROM pg_stat_activity
                                WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%'
                                AND query not like 'SET application_name%' AND state != 'idle'
@@ -14,21 +14,21 @@ if __name__ == "__main__":
     action_result = execute_sql_query(long_running_query_check, Config.DB_HOST_ACTION,
                                       Config.DB_ACTION_CERTIFICATES)
 
-    for pid, age, usename, query, state in case_result:
+    for pid, age, usename, state in case_result:
         if age:
             print(json.dumps(
-                {'pid': pid, 'age': age.seconds, 'usename': usename, 'query': query, 'DB': 'Case', 'state': state},
+                {'pid': pid, 'age': age.seconds, 'usename': usename, 'DB': 'Case', 'state': state},
                 default=str))
         else:
-            print(json.dumps({'pid': pid, 'age': age, 'usename': usename, 'query': query, 'DB': 'Case', 'state': state},
+            print(json.dumps({'pid': pid, 'age': age, 'usename': usename, 'DB': 'Case', 'state': state},
                              default=str))
 
     for pid, age, usename, query, state in action_result:
         if age:
             print(json.dumps(
-                {'pid': pid, 'age': age.seconds, 'usename': usename, 'query': query, 'DB': 'Action', 'state': state},
+                {'pid': pid, 'age': age.seconds, 'usename': usename, 'DB': 'Action', 'state': state},
                 default=str))
         else:
             print(
-                json.dumps({'pid': pid, 'age': age, 'usename': usename, 'query': query, 'DB': 'Action', 'state': state},
+                json.dumps({'pid': pid, 'age': age, 'usename': usename, 'DB': 'Action', 'state': state},
                            default=str))
